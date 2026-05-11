d0a85b95659cf93e99755e44742fbb3dcbd13846
Ryench1n <164470877+Ryench1n@users.noreply.github.com>
2026-05-12 04:53:29 +0800

    eco

--- src/app/components/ui/calendar.tsx
+++ src/app/components/ui/calendar.tsx
@@ -64,6 +64,11 @@ function Calendar({
           const names = ["Ня", "Да", "Мя", "Лх", "Пү", "Ба", "Бя"];
           return names[day.getDay()];
         },
+        formatCaption: (month) => {
+          const months = ["1-р сар", "2-р сар", "3-р сар", "4-р сар", "5-р сар", "6-р сар",
+                          "7-р сар", "8-р сар", "9-р сар", "10-р сар", "11-р сар", "12-р сар"];
+          return `${month.getFullYear()} оны ${months[month.getMonth()]}`;
+        },
       }}
       components={{
         IconLeft: ({ className, ...props }) => (
